30e79d1ce73b711c01f06cf610c0582d98e51e02
Mikko Silvennoinen <mikko.silvennoinen@stt-lehtikuva.fi>
2025-09-24 07:58:31 +0300

    uibuild

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Osa 3, tehtävät 3.11. <br/>
 # Pohjalla puhelinluettelo-backend johon on kopioitu frontendin dist -hakemisto ja määritelty NodeJS <br/>
-# näyttämään staattinen sisältö sieltä <br/>
-# URL: 
+# näyttämään staattinen sisältö sieltä <br/><br/>
+# URL: https://puhelinluettelo-production.onrender.com
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
+//const cors = require('cors')
 const app = express()
 
 const generateId = () => {
@@ -10,7 +10,7 @@ const generateId = () => {
 
 app.use(express.json())
 app.use(express.static('dist'))
-app.use(cors())
+//app.use(cors())
 
 morgan.token('body', request => {
     return JSON.stringify(request.body)
