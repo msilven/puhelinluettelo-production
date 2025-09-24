045d158e37187f71839aeb41b1b6527294c0c7db
Mikko Silvennoinen <mikko.silvennoinen@stt-lehtikuva.fi>
2025-09-24 07:11:04 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Osa 3, tehtävät 3.11. <br/>
+# Pohjalla puhelinluettelo-backend johon on kopioitu frontendin dist -hakemisto ja määritelty NodeJS <br/>
+# näyttämään staattinen sisältö sieltä <br/>
+# URL: 
---
+++ index.js
@@ -0,0 +1,136 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
+
+const generateId = () => {
+    const id = Math.floor(Math.random() * 1000)
+    return id
+}
+
+app.use(express.json())
+app.use(express.static('dist'))
+app.use(cors())
+
+morgan.token('body', request => {
+    return JSON.stringify(request.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+let persons = [
+    {
+        id: "1",
+        name: "Arto Hellas",
+        number: "040-123456"
+    },
+    {
+        id: "2",
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
+    },
+    {
+        id: "3",
+        name: "Dan Abramov",
+        number: "12-43-234345"
+    },
+    {
+        id: "4",
+        name: "Mary Poppendieck",
+        number: "39-23-6423122"
+    }
+]
+
+app.get('/', (request,response) => {
+    response.send('<h1>Heippa</h1>')
+})
+
+app.get('/info', (request,response) => {
+    const personCount = persons.length
+    const date = new Date()
+
+    const dateTimeStr = `${date.toDateString()} ${date.toTimeString()}`
+    const html =    `<p>Phonebook has info for ${personCount} people</p>
+                    <p>${dateTimeStr}</p>`
+
+    //console.log(dateTimeStr)
+
+    response.send(html)
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    // If name is missing give error
+    if(!body.name) {
+        return response.status(400).json({
+            error: "Name is missing!"
+        })
+    }
+
+    // If number is missing give error
+    if(!body.number) {
+        return response.status(400).json({
+            error: "Number is missing!"
+        })
+    }
+
+    // If name is already in the list give error
+    const nameCheck = persons.find((element) => element.name === body.name)
+    if(nameCheck) {
+        return response.status(400).json({
+            error: "Name already exists"
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId().toString(),
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+app.get('/api/persons', (request,response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request,response) => {
+    const id = request.params.id
+	const person = persons.find(person => person.id === id)
+
+	if(person) {
+		response.json(person)
+	} else {
+		response.status(404).end()
+	}
+})
+
+app.put('/api/persons/:id', (request,response) => {
+    const id = request.params.id
+    const body = request.body
+
+    const person = persons.find(person => person.id === id)
+
+    if(person) {
+        person.name = body.name
+        person.number = body.number
+        response.json(person)    
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request,response) => {
+    const id = request.params.id
+	persons = persons.filter(person => person.id !== id)
+
+	response.status(204).end()
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
