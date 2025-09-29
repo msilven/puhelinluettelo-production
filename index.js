f10b1540394da68ed62f687f21a730d79bc73775
Mikko Silvennoinen <mikko.silvennoinen@stt-lehtikuva.fi>
2025-09-29 22:06:11 +0300

    uibuild

--- index.js
+++ index.js
@@ -22,36 +22,17 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 let persons = []
 
-/*
-let persons = [
-    {
-        id: "1",
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: "2",
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: "3",
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: "4",
-        name: "Mary Poppendieck",
-        number: "39-23-6423122"
-    }
-]
-*/
-
 app.get('/', (request,response) => {
     response.send('<h1>Heippa</h1>')
 })
 
 app.get('/info', (request,response) => {
+
+    Person.find({}).then(persons => {
+        response.json(persons)
+    }) else {
+        response.status(404).end()
+    }
 
     const personCount = persons.length
     const date = new Date()
@@ -116,7 +97,9 @@ app.get('/api/persons/:id', (request,response) => {
 
     Person.findById(request.params.id).then(person => {
         response.json(person)
-    })
+    }) else {
+		response.status(404).end()
+	}
 /*
     const person = persons.find(person => person.id === id)
 
